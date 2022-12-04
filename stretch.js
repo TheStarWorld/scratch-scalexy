d107f7ee4f5d767bde1516301893434aae2294dd
Star World <96512249+TheStarWorld@users.noreply.github.com>
2022-12-03 23:57:07 -0700

    shortened

--- stretch.js
+++ stretch.js
@@ -36,9 +36,12 @@
         if (util.target.renderer) {
 			const props = {};
 			
-			const renderedDirectionScale = scaledir(args,util);
+			let finalScale = [util.target.size * util.target.scalex / 100, util.target.size * util.target.scaley / 100];
+			if (util.target.rotationStyle === 'left-right' && util.target.direction < 0) {
+				finalScale[0] *= -1;
+			}
 			
-			props.scale = renderedDirectionScale.scale;
+			props.scale = finalScale;
 			
 			util.target.renderer.updateDrawableProperties(util.target.drawableID, props);
 			
@@ -53,35 +56,3 @@
 
   Scratch.extensions.register(new morescale());
 })(Scratch);
-
-function ROTATION_STYLE_LEFT_RIGHT () {
-	return 'left-right';
-}
-
-function ROTATION_STYLE_NONE () {
-	return "don't rotate";
-}
-
-clamp = function (a, low, high) {
-  if (a === undefined) a = 0;
-  if (low === undefined) low = 0;
-  if (high === undefined) high = 0;
-  return a < low ? low : a > high ? high : a;
-}
-
-scaledir = function (args, util) {
-	// Default: no changes to `this.direction` or `this.scale`.
-	let finalDirection = util.target.direction;
-	let finalScale = [util.target.size * util.target.scalex / 100, util.target.size * util.target.scaley / 100];
-	if (util.target.rotationStyle === ROTATION_STYLE_NONE()) {
-		// Force rendered direction to be 90.
-		finalDirection = 90;
-	} else if (util.target.rotationStyle === ROTATION_STYLE_LEFT_RIGHT()) {
-		// Force rendered direction to be 90, and flip drawable if needed.
-		finalDirection = 90;
-		if (util.target.direction < 0) {
-			finalScale[0] *= -1;
-		}
-	}
-	return {direction: finalDirection, scale: finalScale};
-}
